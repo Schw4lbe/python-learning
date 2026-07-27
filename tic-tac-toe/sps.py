f2ec270df1a3c7f71eeb26c246d7b0fea8e1ff75
Schw4lbe <admin@tapemate.onmicrosoft.com>
2026-07-27 14:05:25 +0200

    updated tictactoe created todo dir

---
+++ tic-tac-toe/sps.py
@@ -0,0 +1,129 @@
+import random
+import math
+from textwrap import dedent
+
+
+class GameData:
+    def __init__(self):
+        self.user_select: int = 0
+        self.user_rounds_won: int = 0
+        self.user2_select: int = 0
+        self.user2_rounds_won: int = 0
+        self.winner: int = 0
+
+    OPTIONS: dict = {
+        1: {"name": "stone", "icon": "\u26ab"},
+        2: {"name": "paper", "icon": "\U0001f4c4"},
+        3: {"name": "scissors", "icon": "\u2702\ufe0f"},
+    }
+
+
+game_data = GameData()
+
+
+def init_game():
+    reset_game_data()
+
+    try:
+        play_turn()
+
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+        exit()
+
+
+def reset_game_data():
+    game_data.__init__()
+
+
+def get_win_condition(turns):
+    return math.ceil(turns / 2)
+
+
+def play_turn():
+    check_win_condition()
+    if game_data.winner:
+        clear_console()
+        return game_data.winner
+
+    print(dedent(f"""
+        New Round, select your option:
+        1 = {game_data.OPTIONS[1]["name"]} {game_data.OPTIONS[1]["icon"]}
+        2 = {game_data.OPTIONS[2]["name"]} {game_data.OPTIONS[2]["icon"]}
+        3 = {game_data.OPTIONS[3]["name"]} {game_data.OPTIONS[3]["icon"]}
+    """))
+    set_turn_options()
+
+
+def clear_console():
+    # \033[2J → clear the terminal screen
+    # \033[H → move cursor to the top-left
+    # end="" → don't add another newline
+    print("\033[2J\033[H", end="")
+
+
+def set_turn_options():
+    game_data.user_select = get_user_select("player1")
+    game_data.user2_select = get_user_select("player2")
+    compare_select(game_data.user_select, game_data.user2_select)
+
+
+def check_win_condition():
+    if game_data.winner:
+        return
+
+    if game_data.user_rounds_won == 1:
+        print("PLAYER1 STARTS !")
+        game_data.winner = 1
+        return
+
+    elif game_data.user2_rounds_won == 1:
+        print("PLAYER2 STARTS !")
+        game_data.winner = 2
+        return
+
+
+def get_user_select(player: str):
+    print(f"{player} select: ")
+    while True:
+        try:
+            user_input = int(input("type 1, 2 or 3: "))
+            if user_input >= 4 or user_input <= 0:
+                print("invalid number.")
+                continue
+            else:
+                return user_input
+
+        except (KeyError, ValueError):
+            print("no valid value.")
+
+
+def get_ai_select():
+    return random.randint(1, 3)
+
+
+def compare_select(player1_option, player2_option):
+    if player1_option == player2_option:
+        print("DRAW, SELECT AGAIN\n")
+        set_turn_options()
+
+    elif get_option_counter(player1_option) == player2_option:
+        game_data.user2_rounds_won += 1
+
+    else:
+        game_data.user_rounds_won += 1
+
+    play_turn()
+
+
+# circular motion
+def get_option_counter(current):
+    return (current % len(game_data.OPTIONS)) + 1
+
+
+def main():
+    init_game()
+
+
+if __name__ == "__main__":
+    main()
--- tic-tac-toe/tic_tac_toe.py
+++ tic-tac-toe/tic_tac_toe.py
@@ -1,5 +1,161 @@
+# two players alternating
+# 3 cols 3 rows
+# Xs and Os as symbols
+# print board after each move
+# check condition every move
+# endgame on win or board full
+
+
+# optional:
+# add scoring for multiple rounds
+# allow new game start without restart
+# allow different board sizes
+import sps
+from colorama import Fore, Style
+
+
+class Player:
+    def __init__(self, name: str):
+        self.name = name
+        self.player_token: str = ""
+        self.player_set_tokens: list = []
+        self.is_starting_player: bool = False
+
+
+class GameData:
+    def __init__(self):
+        self.starting_player: int = 0
+        self.token_first_select: str = "X"
+        self.token_second_select: str = "O"
+        self.turn_indicator: int = 0
+        self.win_conditions: list = [
+            (7, 8, 9),
+            (4, 5, 6),
+            (1, 2, 3),
+            (7, 4, 1),
+            (8, 5, 2),
+            (9, 6, 3),
+            (7, 5, 3),
+            (9, 5, 1),
+        ]
+
+        self.board: dict = {
+            "1": 1,
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9,
+        }
+
+    LINE_SEPARATOR: str = "---+---+---"
+
+
+game_data = GameData()
+player1 = Player("Player 1")
+player2 = Player("Player 2")
+
+
+def init_game():
+    try:
+        sps.main()
+        game_data.starting_player = sps.game_data.winner
+
+        if game_data.starting_player:
+            print_board()
+            start_round()
+
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+        exit()
+
+
+def print_board():
+    print(game_data.LINE_SEPARATOR)
+    print(
+        f" {color_token(game_data.board["7"])} | "
+        f"{color_token(game_data.board["8"])} | "
+        f"{color_token(game_data.board["9"])}"
+    )
+    print(game_data.LINE_SEPARATOR)
+    print(
+        f" {color_token(game_data.board["4"])} | "
+        f"{color_token(game_data.board["5"])} | "
+        f"{color_token(game_data.board["6"])}"
+    )
+    print(game_data.LINE_SEPARATOR)
+    print(
+        f" {color_token(game_data.board["1"])} | "
+        f"{color_token(game_data.board["2"])} | "
+        f"{color_token(game_data.board["3"])}"
+    )
+    print(game_data.LINE_SEPARATOR)
+
+
+# define colors by character
+def color_token(token: str) -> str:
+    if token == "X":
+        return f"{Fore.RED}{token}{Style.RESET_ALL}"
+    elif token == "O":
+        return f"{Fore.BLUE}{token}{Style.RESET_ALL}"
+    else:
+        return f"{Fore.LIGHTBLACK_EX}{token}{Style.RESET_ALL}"
+
+
+def start_round():
+    assign_tokens_and_order()
+
+    first_player = player1 if player1.is_starting_player else player2
+    second_player = player2 if player1.is_starting_player else player1
+
+    while True:
+        player_set_token(first_player)
+        check_win_condition()
+
+        player_set_token(second_player)
+        check_win_condition()
+
+
+def assign_tokens_and_order():
+    if game_data.starting_player == 1:
+        player1.player_token = game_data.token_first_select
+        player2.player_token = game_data.token_second_select
+        player1.is_starting_player = True
+
+    elif game_data.starting_player == 2:
+        player1.player_token = game_data.token_second_select
+        player2.player_token = game_data.token_first_select
+        player2.is_starting_player = True
+
+
+def player_set_token(player: Player):
+    while True:
+        position: str = input(f"{player.name} select your token position: ")
+        if game_data.board[position] not in ("X", "O"):
+            game_data.board[position] = player.player_token
+            player.player_set_tokens.append(position)
+            print_board()
+            break
+        else:
+            print("position allready set, choose a different one.")
+
+
+def check_win_condition():
+    print("checking win condition - TBD")
+
+
+def clear_console():
+    # \033[2J → clear the terminal screen
+    # \033[H → move cursor to the top-left
+    # end="" → don't add another newline
+    print("\033[2J\033[H", end="")
+
+
 def main():
-    print("Hello from tic-tac-toe!")
+    init_game()
 
 
 if __name__ == "__main__":
