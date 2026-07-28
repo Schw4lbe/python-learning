92a4460e311355e0bc783e6ee54a9a11a4a87d5f
Schw4lbe <admin@tapemate.onmicrosoft.com>
2026-07-28 22:30:35 +0200

    finished to do app.

--- todo-app/todo-app.py
+++ todo-app/todo-app.py
@@ -26,9 +26,11 @@ def get_task_data() -> list:
 
 
 def save_to_file(task_manager: Manager):
+    clear_console()
     try:
         with open("data.json", mode="w", encoding="utf-8") as write_file:
             json.dump(task_manager.task_list, write_file)
+        print("SAVED SUCCESSFULLY!")
 
     except FileNotFoundError:
         print("data.json not found.")
@@ -37,44 +39,94 @@ def save_to_file(task_manager: Manager):
 
 
 def init_task_manager():
+    clear_console()
     task_manager = Manager(get_task_data())
+    display_tasks(task_manager)
     select_option(task_manager)
 
 
 def select_option(task_manager: Manager):
     input_options: str = (
-        "(1) show tasks\n(2) add task\n(3) check task\n(4) delete task\n(5) end task manager\nSelect: "
+        "\n(1) add task\n(2) toggle task done\n(3) delete task\n(4) end task manager\nSelect: "
     )
 
     while True:
         user_input: str = input(input_options)
         match user_input:
             case "1":
-                display_tasks(task_manager)
-            case "2":
                 create_new_task(task_manager)
-                display_tasks(task_manager)
+            case "2":
+                toggle_task_done(task_manager)
             case "3":
-                print("TBD complete")
+                delete_task(task_manager)
             case "4":
-                print("TBD delete")
-            case "5":
                 exit_task_manager()
             case _:
-                print("else")
+                print("please select one option.")
+
+
+def delete_task(task_manager: Manager):
+    while True:
+        max_index: int = len(task_manager.task_list) - 1
+        user_input: str = input("select task to toggle: ")
+        if check_abort_operation(user_input):
+            return
+
+        if int(user_input) <= 0 or int(user_input) > max_index:
+            print("select a valid number.")
+            continue
+
+        else:
+            del task_manager.task_list[user_input - 1]
+            save_to_file(task_manager)
+            break
+
+
+def toggle_task_done(task_manager: Manager):
+    while True:
+        max_index: int = len(task_manager.task_list) - 1
+        user_input: str = input("select task to toggle: ")
+        if check_abort_operation(user_input):
+            return
+
+        if int(user_input) <= 0 or int(user_input) > max_index:
+            print("select a valid number.")
+            continue
+
+        else:
+            task_manager.task_list[user_input - 1]["is_completed"] = (
+                not task_manager.task_list[user_input - 1]["is_completed"]
+            )
+            save_to_file(task_manager)
+            break
+
+
+def create_new_task(task_manager: Manager):
+    id: str = str(uuid.uuid4())
+
+    description: str = input("Enter task description: ")
+    if check_abort_operation(description):
+        return
+
+    category: str = input("Enter category: ")
+    if check_abort_operation(category):
+        return
+
+    new_task: Task = Task(id, description, category)
+    add_task_to_manager(task_manager, new_task.__dict__)
 
 
 def exit_task_manager():
-    # TODO: add all needed steps for clear exit out
-    exit()
-
-
-def create_new_task(task_manager: Manager) -> dict:
-    id: str = str(uuid.uuid4())
-    description: str = input("Enter task description: ")
-    category: str = input("Enter category: ")
-    new_task: Task = Task(id, description, category)
-    add_task_to_manager(task_manager, new_task.__dict__)
+    while True:
+        user_confirm: str = input("quit? (y/n): ")
+        if user_confirm == "y":
+            clear_console()
+            exit()
+        elif user_confirm == "n":
+            break
+        else:
+            print("select y or n.")
+            continue
 
 
 def add_task_to_manager(task_manager: Manager, new_task: dict):
@@ -83,12 +135,20 @@ def add_task_to_manager(task_manager: Manager, new_task: dict):
 
 
 def display_tasks(task_manager: Manager):
-    # TODO: update to hide id and show index instead for task operations
-    # consider using a enumeration
-    for task in task_manager.task_list:
-        print(task)
+    for index, task in enumerate(task_manager.task_list):
+        print(f"({index}) {task}")
 
-    select_option(task_manager)
+
+def clear_console():
+    # \033[2J → clear the terminal screen
+    # \033[H → move cursor to the top-left
+    # end="" → don't add another newline
+    print("\033[2J\033[H", end="")
+
+
+def check_abort_operation(input: str) -> bool:
+    if input == "!a":
+        return True
 
 
 def main():
