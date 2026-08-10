8c6fc8ec97080a1b2ccbd991dd58cfed852ecc39
Schw4lbe <admin@tapemate.onmicrosoft.com>
2026-08-10 15:17:16 +0200

    solved second study example.

--- _own-exercises/02-functional-programming/main.py
+++ _own-exercises/02-functional-programming/main.py
@@ -28,10 +28,10 @@ users = [
 ]
 
 1. LAMBDA
-   Create a lambda that formats one user as "Alice (25)".
+   Create a lambda that formats the first user as "Alice (25)".
 
 2. MAP
-   Use map() to create the formatted user list for the admin dashboard.
+   Use map() to create a formatted users list (name: __ age: __ active: __) for the admin dashboard.
 
 3. FILTER
    Use filter() to find users who are adults AND have an active account.
@@ -50,9 +50,84 @@ it when needed, transforms it for display, filters it for an admin
 operation, and checks whether a relevant user exists..
 """
 
+users: list[tuple] = [
+    ("Alice", 25, True),
+    ("Bob", 17, False),
+    ("Charlie", 32, True),
+    ("Diana", 15, True),
+]
+
+users_rebuild: list[tuple] = []
+
+names: list[str] = []
+ages: list[int] = []
+states: list[bool] = []
+
+
+def init_admin_tool():
+    lambda_reformat_example(users[0])
+    users_mapped: list = list(map(mapping_function, users))
+    for user in users_mapped:
+        print(user)
+
+    adults = filter(filter_adults, users)
+    print("adult users: ")
+    for user in adults:
+        print(user)
+
+    destructor_tuples_into_lists(users)
+    rebuild_users(names, ages, states)
+    contains_active_adult(users)
+
+
+def contains_active_adult(users: list[tuple]):
+    # any needs some sort of iterable and returns a bool
+    # when any is used on dict it checks keys first, for values use param.values()
+    result: bool = any(user[1] >= 18 and user[2] for user in users)
+
+    if result:
+        print("has adult active user.")
+
+
+def rebuild_users(names: list[str], ages: list[int], states: list[bool]):
+    global users_rebuild
+    users_rebuild = list(zip(names, ages, states))
+    print(users_rebuild)
+
+
+def destructor_tuples_into_lists(users: list[tuple]):
+    for user in users:
+        names.append(user[0])
+        ages.append(user[1])
+        states.append(user[2])
+
+    print(names, ages, states)
+
+
+def filter_adults(user: tuple):
+    if user[1] >= 18:
+        return True
+    else:
+        return False
+
+
+# LEARNING:
+# iterable handed over
+# type hint declares single element in iterable
+# -> list of tuples -> one element = tuple
+# -> hand over string -> one char also type string
+# be aware to use propper formatted iterables to have fitting operations inside map function
+def mapping_function(user: tuple):
+    return f"name: {user[0]}, age: {user[1]}, active: {user[2]}"
+
+
+def lambda_reformat_example(user: tuple):
+    string: str = lambda u: f"{u[0]} ({u[1]})"
+    print(string(user))
+
 
 def main():
-    print("Hello from 02-functional-programming!")
+    init_admin_tool()
 
 
 if __name__ == "__main__":
