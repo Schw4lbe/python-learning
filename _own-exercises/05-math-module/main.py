9cb927e1945e9fb1b8587694bb5dfbc9872cb05f
Schw4lbe <admin@tapemate.onmicrosoft.com>
2026-08-11 13:23:09 +0200

    finished practice 4

--- _own-exercises/05-math-module/main.py
+++ _own-exercises/05-math-module/main.py
@@ -22,16 +22,84 @@ Use math.gcd() to calculate the greatest common divisor.
 Use math.isclose() to compare floating-point values safely.
 Create a calculator function that receives different mathematical operations and returns the calculated result.
 
-optional:
-Extend the calculator with additional math functions and handle invalid mathematical inputs.
-
 Real-world use case:
 Used in engineering software, simulations, game development, scientific applications, financial calculations, data analysis, and systems requiring reliable mathematical operations.
 """
 
+import math
+
+
+def init_calculator():
+    try:
+        user_menu_select()
+    except KeyboardInterrupt:
+        print("exit.")
+
+
+def user_menu_select():
+    print("""
+0 - sqrt
+1 - pow
+2 - ceil / floor
+3 - pi
+4 - sin cos tan
+5 - factorial
+6 - fabs
+7 - gcd (greatest common divisor)
+8 - isclose
+""")
+    user_input: str = input("select calculation: ")
+
+    match user_input:
+        case "0":
+            print(math.sqrt(float(input("sqrt of: "))))
+
+        case "1":
+            num1: float = float(input("num1: "))
+            num2: float = float(input("num2: "))
+            print(math.pow(num1, num2))
+
+        case "2":
+            mode: str = input("ceil (c) or floor (f)?: ")
+            if mode == "c":
+                print(math.ceil(float(input("enter number: "))))
+            elif mode == "f":
+                print(math.floor(float(input("enter number: "))))
+
+        case "3":
+            print(float(input("circle diameter: ")) * math.pi)
+
+        case "4":
+            mode: str = input("sin(s), cos(c), tan(t): ")
+            if mode == "s":
+                print(math.sin(float(input("sin of: "))))
+            elif mode == "c":
+                print(math.cos(float(input("cos of: "))))
+            elif mode == "t":
+                print(math.tan(float(input("tan of: "))))
+
+        case "5":
+            print(math.factorial(int(input("enter int: "))))
+
+        case "6":
+            print(math.fabs(float(input("enter negative number: "))))
+
+        case "7":
+            int1: int = int(input("enter int1: "))
+            int2: int = int(input("enter int2: "))
+            print(math.gcd(int1, int2))
+
+        case "8":
+            number1: float = float(input("enter number1: "))
+            number2: float = float(input("enter number2: "))
+            max: float = float(input("enter tollerance: "))
+            print(math.isclose(number1, number2, rel_tol=max))
+
+    user_menu_select()
+
 
 def main():
-    print("Hello from 05-math-module!")
+    init_calculator()
 
 
 if __name__ == "__main__":
