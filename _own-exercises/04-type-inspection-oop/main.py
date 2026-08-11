0b95fc33a3b4d06bc54f24e8fb709d558f28d4a0
Schw4lbe <admin@tapemate.onmicrosoft.com>
2026-08-11 11:34:23 +0200

    practice 4 done.

--- _own-exercises/04-type-inspection-oop/main.py
+++ _own-exercises/04-type-inspection-oop/main.py
@@ -24,24 +24,71 @@ employees = [
    Create Employee as the base class and Developer, Manager, and Designer as specialized classes.
 
 2. SUPER()
-   Use super() to reuse Employee initialization and add role-specific data.
+   Use super() to reuse Employee initialization.
 
 3. ISSUBCLASS()
    Use issubclass() to verify that a role class inherits from Employee before adding it to the system.
 
 4. ISINSTANCE()
-   Create one processing function that receives any employee and handles it according to its role.
-
-5. WORKFLOW
-   Pass all employees through the same function and produce role-specific output.
+   Create one processing function that receives any employee and handles it according to its role and produce role-specific output.
 
 Goal:
 Build one simple HR workflow where inheritance provides shared behavior and isinstance() determines how each employee is handled.
 """
 
 
+class Employee:
+    def __init__(self, name: str):
+        self.name = name
+
+
+class Developer(Employee):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Manager:
+    def __init__(self, name):
+        # super().__init__(name)
+        self.name = name
+
+
+class Designer(Employee):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+def init_circle():
+    employees: list = create_instances(
+        [Developer("Alice"), Manager("Bob"), Designer("Charlie")]
+    )
+    handle_employees(employees)
+
+
+def handle_employees(employees: list):
+    for employee in employees:
+        if type(employee).__name__ == "Developer" and isinstance(employee, Developer):
+            print(f"{employee.name} is a Developer")
+        elif type(employee).__name__ == "Designer" and isinstance(employee, Designer):
+            print(f"{employee.name} is a Designer")
+        elif type(employee).__name__ == "Manager" and isinstance(employee, Manager):
+            print(f"{employee.name} is a Manager")
+
+
+def create_instances(creation_list: list):
+    print("CHECK INSTANCIATION:")
+    for item in creation_list:
+        if issubclass(type(item), Employee):
+            print(type(item).__name__)
+        else:
+            print(f"{type(item).__name__} (is non Employee subclass.)")
+    print("\n")
+
+    return creation_list
+
+
 def main():
-    print("Hello from 04-type-inspection-oop!")
+    init_circle()
 
 
 if __name__ == "__main__":
